34898f6b7e86c31ff6bb69e6c8fc09998b2fe5f1
FlutLab.io <>
2021-05-01 20:20:40 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(HomePageTemp());
+
 
 class HomePageTemp extends StatelessWidget {
   final opciones = [
