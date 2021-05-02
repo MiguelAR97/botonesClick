29ac8e6ed744d5ed9ee8898d2d2684af0bc1de91
FlutLab.io <>
2021-05-02 17:48:55 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,68 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(HomePageTemp());
  
-class MyApp extends StatelessWidget {
+class HomePageTemp extends StatelessWidget {
+  final opciones = [
+    'opcion1',
+    'opcion2',
+    'opcion3',
+    'opcion4',
+    'opcion5',
+    'opcion6'
+  ];
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Widgets App',
-      debugShowCheckedModeBanner: false,
-      home: HomePageTemp(),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componente Temporal'),
+      ),
+      body: ListView(children: _crearWidgets(context)),
+    );
+  }
+
+  List<Widget> _crearWidgets(BuildContext context) {
+    List<Widget> lista = new List<Widget>();
+    for (String opt in opciones) {
+      final tempWidget = ListTile(
+        title: Text(opt),
+        subtitle: Text('Subtitulo de la ' + opt),
+        leading: Icon(Icons.account_box_rounded),
+        trailing: Icon(Icons.keyboard_arrow_right_outlined),
+        onTap: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) => _buildPopupDialog(context, opt),
+          );
+        },
+      );
+      lista..add(tempWidget)..add(Divider());
+
+      //lista.add(tempWidget);
+      //lista.add(Divider());
+    }
+    return lista;
+  }
+
+  Widget _buildPopupDialog(BuildContext context, String opt) {
+    return new AlertDialog(
+      title: const Text('Me diste click'),
+      content: new Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text("Hola soy la "+opt),
+        ],
+      ),
+      actions: <Widget>[
+        new FlatButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          textColor: Theme.of(context).primaryColor,
+          child: const Text('Close'),
+        ),
+      ],
     );
   }
 }
