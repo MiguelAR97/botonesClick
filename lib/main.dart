9564cb1611da9537a6e081a80a08c81a5814ff9a
FlutLab.io <>
2021-05-02 17:43:36 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -2,37 +2,15 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(HomePageTemp());
 
-class HomePageTemp extends StatelessWidget {
-  final opciones = ['opcion1','opcion2','opcion3','opcion4','opcion5','opcion6'];
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Componente Temporal'),
-      ),
-      body: ListView(
-        children: <Widget>[
-          ListTile(
-            title: Text('Elemento 1'),
-          ),
-          Divider(),
-                    ListTile(
-            title: Text('Elemento 2'),
-          ),Divider(),
-                    ListTile(
-            title: Text('Elemento 3'),
-          ),Divider(),
-                    ListTile(
-            title: Text('Elemento 4'),
-          ),Divider(),
-                    ListTile(
-            title: Text('Elemento 5'),
-          ),Divider(),
-                    ListTile(
-            title: Text('Elemento 6'),
-          ),Divider(),
-        ],
-      ),
+    return MaterialApp(
+      title: 'Widgets App',
+      debugShowCheckedModeBanner: false,
+      home: HomePageTemp(),
     );
   }
 }
