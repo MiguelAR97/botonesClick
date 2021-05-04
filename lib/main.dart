be0f546f86ec4e62c314345072ef8476c76eab96
FlutLab.io <>
2021-05-04 04:05:16 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -2,79 +2,57 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(_MenuProvider());
  
-class _MenuProvider {
-  List<dynamic> generos = [];
-  _MenuProvider() {
-   // cargarData();
-  }
+class _ContadorPageState extends State<ContadorPage> {
+  final _estiloTexto = new TextStyle(fontSize: 32);
 
-  Future <List<dynamic>> cargarData() async{
-    
-    
-    final resp = await rootBundle.loadString('data/menu_opts.json');
-      //print(data);
-      Map dataMap = json.decode(resp);
-      print(dataMap['rutas']);
-      generos = dataMap['rutas'];
-    
-    return generos;
-     final generos = [
-    'Genero 1',
-    'Genero 2',
-    'Genero 3',
-    'Genero 4'
-  ];
+  int _conteo = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Generos de Música'),
-      ),
-      body: ListView(children: _crearWidgets(context)),
+    return MaterialApp(
+        theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+        ),
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('Contador Stateful widget'),
+              centerTitle: true,
+            ),
+            body: Center(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('Numero de Cliks', style: _estiloTexto),
+                Text('$_conteo', style: _estiloTexto),
+              ],
+            )),
+            floatingActionButton: _crearBotones()));
+  }
+
+  Widget _crearBotones() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        SizedBox(width: 30),
+        FloatingActionButton(
+            child: Icon(Icons.exposure_zero), onPressed: _reset),
+        Expanded(child: SizedBox()),
+        FloatingActionButton(child: Icon(Icons.remove), onPressed: __sustraer),
+        SizedBox(width: 5.0),
+        FloatingActionButton(child: Icon(Icons.add), onPressed: _agregar),
+      ],
     );
   }
 
-  List<Widget> _crearWidgets(BuildContext context) {
-    List<Widget> lista = new List<Widget>();
-    for (String opt in generos) {
-      final tempWidget = ListTile(
-        title: Text(opt),
-        subtitle: Text('Subtitulo de la ' + opt),
-        leading: Icon(Icons.account_box_rounded),
-        trailing: Icon(Icons.keyboard_arrow_right_outlined),
-        onTap: () {
-          showDialog(
-            context: context,
-            builder: (BuildContext context) => _buildPopupDialog(context, opt),
-          );
-        },
-      );
-      lista..add(tempWidget)..add(Divider());
-
-      //lista.add(tempWidget);
-      //lista.add(Divider());
-    }
-    return lista;
+  void __sustraer() {
+    setState(() => _conteo--);
   }
 
-  Widget _buildPopupDialog(BuildContext context, String opt) {
-    return new AlertDialog(
-      title: const Text('Me diste click'),
-      content: new Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text("El genero es "+opt),
-        ],
-      ),
-      actions: <Widget>[
-        new FlatButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          textColor: Theme.of(context).primaryColor,
-          child: const Text('Close'),
+  void _agregar() {
+    setState(() => _conteo++);
+  }
+
+  void _reset() {
+    setState(() => _conteo = 0);
   }
 }
-
-final menuProvider = new _MenuProvider();
